599516432f843465c0de19ceabc05bad829ede39
pablo mansilla martinelli <pablo.0698@hotmail.com>
2023-04-12 09:22:29 -0500

    New Games

---
+++ JS/Games/ballons.js
@@ -0,0 +1,25 @@
+class Ballons extends GameTemplate
+{
+    maxSpeed = 15;
+
+    constructor(position, angle, scale)
+    {
+        super(position, angle, scale);
+    }
+    preload()
+    {
+        
+    }
+    start()
+    {
+
+    }
+    update()
+    {
+
+    }
+    input()
+    {
+
+    }
+}
